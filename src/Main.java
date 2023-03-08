c3e3c4525525a0671f4a3b05a6117571c20ebb82
Григорий Бойко <93492134+BGN656@users.noreply.github.com>
2023-03-08 22:31:12 +0200

    C_2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.print("Hello world!\n");
-        System.out.print("World!");
+        System.out.print("World! \n");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите: ");
+        int fid = scan.nextInt();
+        System.out.print("World!  "+ fid);
+
+
     }
 }
