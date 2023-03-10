ed55723219caa64d280ea0fa519a89e6f986cf30
Григорий Бойко <93492134+BGN656@users.noreply.github.com>
2023-03-10 20:29:03 +0200

    C_4

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.print("Hello world!\n");
         System.out.print("World! \n");
+        System.out.println(10+3);
         Scanner scan = new Scanner(System.in);
-        System.out.print("Введите: ");
+        System.out.print("Введите возраст: ");
         int fid = scan.nextInt();
-        System.out.print("World!  " + fid + "  !!!!!");
+        System.out.print("Введите имя: ");
+        String nm = scan.nextLine();
+        nm = scan.nextLine();
+
+        System.out.print("Привет  " + fid + "  !!!!!  " + nm);
+
+
 
 
     }
---
+++ src/MyF.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MyF {
+    public static void main(String[] args) {
+        float gty = 45;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        float dty2 = scan.nextFloat();
+        System.out.print(gty + dty2);
+    }
+}
