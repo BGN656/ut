b7244d53acab17da6be3e439b319d0c53962f807
Григорий Бойко <93492134+BGN656@users.noreply.github.com>
2023-03-11 14:57:41 +0200

    C_8-2

--- src/Lessen1/Main.java
+++ src/Lessen1/Main.java
@@ -2,13 +2,11 @@ package Lessen1;
 
 import java.util.Scanner;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.print("World! \n");
-        System.out.println(10+3);
+        System.out.println(10 + 3);
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите возраст: ");
         int fid = scan.nextInt();
--- src/Lessen2/MyF.java
+++ src/Lessen2/MyF.java
@@ -6,14 +6,20 @@ public class MyF {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Введите 1 число: ");
+
         float dty1 = scan.nextFloat();
-        System.out.print("Введите 2 число: ");
-        float dty2 = scan.nextFloat();
-        System.out.print("Введите математическую операцыю: ");
-        String dty = scan.nextLine();
-        dty = scan.nextLine();
+        float dty2 = 1.45f;
+        System.out.println(dty2 + dty1);
 
+        int i;
+        System.out.print("Введите число 2: ");
+        i = scan.nextInt();
 
-        System.out.print(dty2 + dty1);
+        if (i<6) {
+            System.out.println("Опа"+ "i = "+ i);
+        } else {
+            System.out.println("Памммм");
+        }
+
     }
 }
