fea60388222cb8518e326e5b4e786546a322b6a6
Григорий Бойко <93492134+BGN656@users.noreply.github.com>
2023-03-11 15:30:54 +0200

    C_9

--- src/Lessen1/Main.java
+++ src/Lessen1/Main.java
@@ -4,17 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.print("World! \n");
-        System.out.println(10 + 3);
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите возраст: ");
-        int fid = scan.nextInt();
-        System.out.print("Введите имя: ");
-        String nm = scan.nextLine();
-        nm = scan.nextLine();
+//        System.out.println("Hello world!");
+  //      System.out.print("World! \n");
+//        System.out.println(10 + 3);
+       Scanner scan = new Scanner(System.in);
+//        System.out.print("Введите возраст: ");
 
-        System.out.print("Привет  " + fid + "  !!!!!  " + nm);
+//        int fid = scan.nextInt();
+//        System.out.println("Введите имя: ");
+//        String nm = scan.nextLine();
+//        nm = scan.nextLine();
+//        System.out.print("Привет  " + fid + "  !!!!!  " + nm);
+
+        System.out.print("Введите угол: ");
+        double gr = scan.nextFloat();
+        double rd = Math.toRadians(gr);
+        System.out.println("Синус угла " + gr + " равен " + rd);
 
 
     }
